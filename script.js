30966ab5178e97481c93936d0e4356996dd67344
Paul Vargas <videoblogpaul@gmail.com>
2023-01-07 22:09:21 +0100

    ausa por el trabajo

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 
-console.log('Hola mundo')
+const h1 = document.querySelector('h1')// Seleccionar algo
+const p = document.querySelector('p')
+const parrafito = document.querySelector('.parrafito')
+const pid = document.querySelector('#pid')
+const input = document.querySelector('input')
+
+console.log({h1,p,parrafito, pid, input})
+console.log(input.value)
